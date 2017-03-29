4d4392a47dde81f337f25971abb501bed8fd1c58
namclu <namlu1205@gmail.com>
2017-03-29 14:43:50 +1100

    0: Initial commit

---
+++ app/src/main/java/com/namclu/android/justbooks/MainActivity.java
@@ -0,0 +1,13 @@
+package com.namclu.android.justbooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
