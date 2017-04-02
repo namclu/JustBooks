97035ca0a2582d34e9b330d66caecfda5e4250f9
namclu <namlu1205@gmail.com>
2017-04-02 17:38:27 +1000

    1: Extract text in SearchView and display in Toast msg
    After entering text and hitting Search button, Toast msg displays
    the text entered and then launches SearchResultsActivity.
    Set query hint inside of SearchView.

--- app/src/main/java/com/namclu/android/justbooks/MainActivity.java
+++ app/src/main/java/com/namclu/android/justbooks/MainActivity.java
@@ -1,13 +1,16 @@
 package com.namclu.android.justbooks;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private SearchView searchField;
     private Button searchButton;
 
     @Override
@@ -16,7 +19,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         // Initialize views
+        searchField = (SearchView) findViewById(R.id.search_main_search_field);
         searchButton = (Button) findViewById(R.id.button_main_search);
+
+        // Set views
+        searchField.setQueryHint(getResources().getString(R.string.search_hint));
 
         // Set onClickListener
         searchButton.setOnClickListener(this);
@@ -24,6 +31,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        Toast.makeText(this, "Search button pressed", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Searching for " + searchField.getQuery(), Toast.LENGTH_SHORT).show();
+
+        Intent intent  = new Intent(this, SearchResultsActivity.class);
+        startActivity(intent);
     }
 }
