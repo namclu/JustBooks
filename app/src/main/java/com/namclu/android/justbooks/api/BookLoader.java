8d44937351c99fad246b0bc8309253a14cd6866b
namclu <namlu1205@gmail.com>
2017-04-05 15:44:55 +1000

    3: Add BookLoader.java

---
+++ app/src/main/java/com/namclu/android/justbooks/api/BookLoader.java
@@ -0,0 +1,36 @@
+package com.namclu.android.justbooks.api;
+
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.namclu.android.justbooks.api.models.Book;
+
+import java.util.List;
+
+/**
+ * Created by namlu on 05-Apr-17.
+ *
+ * Loads a list of books by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+
+public class BookLoader extends AsyncTaskLoader<List<Book>> {
+
+    private String mUrl;
+
+    public BookLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    public List<Book> loadInBackground() {
+
+        // Submit task only if URL entry is not null
+        if (mUrl == null) {
+            return null;
+        }
+        return QueryUtils.fetchBookData(mUrl);
+    }
+}
